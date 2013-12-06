60dc831b758e755048a041415655f52670a413cc
jbub <git@jbub.eu>
2013-12-06 21:11:12 +0100

    initial commit

---
+++ js/lang.js
@@ -0,0 +1,18 @@
+// Localized user interface.
+var tr = {
+	"off": "vypnuté",
+	"on": "zapnuté",
+	"syntax": "Zvýrazňovanie syntaxe",
+	"lineno": "Číslovanie riadkov",
+	"reset": "Obnoviť slajd",
+	"format": "Formátovať zdrojový kód",
+	"kill": "Zabiť program",
+	"run": "Spustiť",
+	"compile": "Skompilovať a spustiť",
+	"more": "Možnosti",
+	"toc": "Obsah",
+	"prev": "Predchádzajúci",
+	"next": "Nasledujúci",
+	"waiting": "Čakanie na vzdialený server...",
+	"errcomm": "Chyba pri komunikácii so vzdialeným serverom.",
+};
